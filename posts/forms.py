677a3f5532ba1dbb00f450b379914446b13d6a34
KirillDegtyarev <lirik19@yandex.ru>
2023-08-12 00:02:31 +0700

    new

--- posts/forms.py
+++ posts/forms.py
@@ -6,28 +6,34 @@ from django import forms
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['image', 'body', 'tags']
         labels = {
-            'body': 'Caption',
+            'body': 'Комментарий',
+            'tags': 'Категории'
         }
         widgets = {
             'body': forms.Textarea(attrs={
                 'rows': 3,
-                'placeholder': 'Add a caption...',
+                'placeholder': 'Добавьте Ваш комментарий...',
                 'class': 'font1 text-4xl'}),
+            'image': forms.TextInput(attrs={
+                'placeholder': 'Укажите адрес картинки...'}),
+            'tags': forms.CheckboxSelectMultiple(),
         }
 
 
 class PostEditForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['body', ]
+        fields = ['body', 'tags']
         labels = {
             'body': '',
+            'tags': 'Категории',
         }
         widgets = {
             'body': forms.Textarea(attrs={
                 'rows': 3,
                 'class': 'font1 text-4xl'}),
+            'tags': forms.CheckboxSelectMultiple(),
         }
 
--- posts/models.py
+++ posts/models.py
@@ -6,12 +6,21 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     image = models.URLField(max_length=500)
     body = models.TextField()
+    tags = models.ManyToManyField('Tag')
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=100, default=uuid.uuid4,
                           unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.title)
-    
+
     class Meta:
         ordering = ['created']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
